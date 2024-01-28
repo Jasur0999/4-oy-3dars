5cbc8d0f2cf3f6040328bf8f783e54278666cacb
Jasur0999 <leaderjasur@gmail.com>
2024-01-28 19:54:11 +0500

    round

---
+++ main.js
@@ -0,0 +1,363 @@
+//  1 - task   ////////////////////////////////////////////
+
+// let a = 2;
+// if ( a >= 1) {
+//     console.log( ++a );
+
+// } else {
+//     console.log( a );
+
+// }
+
+//  2 - task   ////////////////////////////////////////////
+
+// let n = -1;
+// if ( n >= 5 ) {
+//     console.log( ++n );
+// }
+// if ( n <= -1 ) {
+//     console.log( n - 2);
+// }
+// if ( n == 0) {
+//     console.log( n = 10);
+// }
+
+//  3 - task   ////////////////////////////////////////////
+
+// let a = 32;
+// let b = 40;
+// let c = 52;
+// console.log( a >= 0 && b >= 0 && c >= 0);
+
+//  4 - task   ////////////////////////////////////////////
+
+// let a = 100;
+// let b = 124;
+// if ( a > b) {
+//     console.log( a );
+// }
+// else{
+//     console.log( b );
+// }
+
+//  5 - task   ////////////////////////////////////////////
+
+/*  let a = 20;
+let b = 30;
+let y1;
+let y2;
+if (a) {
+    if ( a > b) {
+        console.log( b );
+    }
+    else {
+        console.log(a)
+    }
+    
+    y1 = a / 10;
+    y2 = a % 10;
+    if ( y1 < y2) {
+        console.log((y1, y2))
+        // console.log(y1)
+    }
+    else {
+        console.log(y2, y1);
+    }
+    
+   
+}
+
+else {
+    if ( a < b) {
+        console.log( a );
+    }
+    else {
+        console.log(b)
+    }
+    
+    y1 = b / 10;
+    y2 = b % 10;
+    if ( y1 < y2) {
+        console.log((y1, y2))
+        // console.log(y1)
+    }
+    else {
+        console.log(y2, y1);
+    }
+}       */
+
+//  6 - task   ////////////////////////////////////////////
+
+// let n = 84;
+// let m = 56;
+// if ( n < m ) {
+//     console.log(m);
+// }
+// else {
+//     console.log(n);
+// }
+
+//  7 - task   ////////////////////////////////////////////
+
+// let a = -36;
+// let b = -27;
+// let c = 24;
+// console.log( a + c, b + c );
+
+//  8 - task   ////////////////////////////////////////////
+
+// let k = 34;
+// let l = 72;
+// if ( k != l ) {
+//     console.log( k + l );
+// }
+// if ( k == l) {
+//     console.log( k = 0, l = 0 );
+// }
+
+// else {
+//     console.log('Son kiritilmagan')
+// }
+
+//  9 - task   ////////////////////////////////////////////
+
+// let a = 93;
+// let b = 68;
+// if ( a != b) {
+//     if ( a < b) {
+//         a = b;
+//         console.log( a, b);
+//     }
+//     else {
+//         b = a;
+//         console.log( a, b);
+//     }
+// }
+
+// else {
+//     if ( a == b) {
+//         a = 0;
+//         b = 0;
+//         console.log( a, b );
+//     }
+// }
+
+//  1 - task   ////////////////////////////////////////////
+
+/*  let yakuniyBaho = 15;
+let baho;
+switch (yakuniyBaho) {
+    case 1:
+        baho = 'yomon';
+        break;
+    case 2:
+        baho = 'qoniqarsiz';
+        break;
+    case 3:
+        baho = 'qoniqarli';
+        break;
+    case 4:
+        baho = 'yaxshi';
+        break;
+    case 5:
+        baho = 'alo';
+        break;
+  default:
+    console.log('Imtixonga kelmagan')
+    
+}
+console.log(baho);  */
+
+//  2 - task   ////////////////////////////////////////////
+
+/*  let fullMonth = 5;
+let month;
+switch (fullMonth) {
+    case 1:
+        month = 'qish';
+        break;
+    case 2:
+        month = 'qish';
+        break;
+    case 3:
+        month = 'qish';
+        break;
+    case 4:
+        month = 'bahor';
+        break;
+    case 5:
+        month = 'bahor';
+        break;
+    case 6:
+        month = 'bahor';
+        break;
+    case 7:
+        month = 'yoz';
+        break;
+    case 8:
+        month = 'yoz';
+        break;
+    case 9:
+        month = 'yoz';
+        break;
+    case 10:
+        month = 'kuz';
+        break;
+    case 11:
+        month = 'kuz';
+        break;
+    case 12:
+        month = 'kuz';
+        break;
+    default:
+        console.log('Bunday oy yoq');
+}
+ 
+console.log(month);    */
+
+//  3 - task   ////////////////////////////////////////////
+
+/*  let monthday = 7;
+let day;
+switch (monthday) {
+    case 1:
+        day = '31 - kun ';
+        break;
+    case 2:
+        day = '28 - kun ';
+        break;
+    case 3:
+        day = '31 - kun ';
+        break;
+    case 4:
+        day = '30 - kun ';
+        break;
+    case 5:
+        day = '31 - kun ';
+        break;
+    case 6:
+        day = '30 - kun ';
+        break;
+    case 7:
+        day = '31 - kun ';
+        break;
+    case 8:
+        day = '30 - kun ';
+        break;
+    case 9:
+        day = '31 - kun ';
+        break;
+    case 10:
+        day = '30 - kun ';
+        break;
+    case 11:
+        day = '31 - kun ';
+        break;
+    case 12:
+        day = '31 - kun ';
+        break;
+        default:
+            console.log( ' Ortiqcha  kun yoq ')
+}
+console.log(day);   */
+
+//  4 - task   ////////////////////////////////////////////
+
+/*  let m = +prompt("Metr kiriting: ");
+let dm = +prompt("Detsimetr kiriting: ");
+let km = +prompt("Kilometr kiriting: ");
+let mm = +prompt("MILLIMETR kiriting: ");
+let sm = +prompt("santimetr kiriting: ");
+let metrlar = prompt(
+  "birlikni kiriting: (dm, santimetr, kilometr, millimetr, metr)"
+);
+
+switch (metrlar) {
+  case "dm":
+    dm /= 10;
+    console.log(dm);
+    break;
+    case "sm":
+        sm /= 100;
+        console.log(sm);
+        break;
+    case "km":
+        km /= 1000;
+        console.log(km);
+        break;
+    case "mm":
+        mm *= 1000;
+        console.log(mm);
+        break;
+    default:
+        console.log('Uzunlik kiritilmagan');
+}
+console.log(metrlar);   */
+
+
+//  5 - task   ////////////////////////////////////////////
+
+/*  let kg = +prompt('kilogram kiriting');
+let mlg = +prompt('milligram kiriting');
+let gr = +prompt('gramm kiriting');
+let tn = +prompt('tonna kiriting');
+let sn = +prompt('sentner kiriting');
+let fullkilogram = prompt("Og'irlikni kiriting (kg, mlg, gr, tn, sn )");
+
+switch (fullkilogram) {
+    case "mlg":
+        mlg *= 10;
+        console.log(mlg);
+        break;
+    case "gr":
+        gr *= 100;
+        console.log(gr);
+        break;
+    case "tn":
+        tn /= 1000;
+        console.log(tn);
+        break;
+    case "sn":
+        sn /= 100;
+        console.log(sn);
+        break;
+    default:
+        console.log('Ogirlik kiritilmagan');
+        
+}
+console.log(fullkilogram);  */
+
+
+//  6 - task   ////////////////////////////////////////////
+
+/*  let musbatToq = +prompt('Musbat toq son');
+let musbatJuft = +prompt('Musbat juft son');
+let nol = +prompt('Nol son');
+let manfiyJuft = +prompt('Manfiy juft son');
+let manfiyToq = +prompt('Manfiy toq son');
+let number = prompt('Round');
+switch (number) {
+    case 'musbatToq':
+        (musbatToq % 2 ) - 1 ;
+        break ;
+    case 'musbatJuft':
+        musbatJuft % 2 ;
+        break ;
+    case 'nol':
+        nol = 0;
+        break;
+    case 'manfiyJuft':
+        manfiyJuft % 2;
+        break;
+    case 'manfiyToq':
+        (manfiyToq % 2) - 1 ;
+        break;
+    default:
+        console.log('Topilmadi');
+}
+console.log(musbatToq, musbatJuft, nol, manfiyJuft, manfiyToq);  */
+
+
+
+
+
